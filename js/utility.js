1fe235c9596ced4bd8ee4c73751c41ad64e913e9
Your Name git config --global user.email you@example.com <you@example.com>
2024-03-05 13:33:12 +0600

    added title part of let discus part and latest port part

--- js/utility.js
+++ js/utility.js
@@ -1,18 +1,18 @@
-const displayAllPosts = (allPosts) =>{
-    //  console.log(allPosts)
-    const latestPostContainer = document.getElementById('left-container');
-    allPosts.forEach((singlePost) => {
-        console.log(singlePost)
-        // Div Create 
-       const cardDiv2 = document.createElement('div')
-      // div inner HTML
+const displayAllPosts = (allPosts) => {
+  //  console.log(allPosts)
+  const latestPostContainer = document.getElementById('left-container');
+  allPosts.forEach((singlePost) => {
+    console.log(singlePost)
+    // Div Create 
+    const cardDiv2 = document.createElement('div')
+    // div inner HTML
     //   console.log(cardDiv2);
-      cardDiv2.innerHTML=`
+    cardDiv2.innerHTML = `
       <div class="hero p-2 border-4 rounded-3xl bg-base-200">
         <div class="hero-content flex-col lg:flex-row gap-10">
 
             <div class="indicator">
-            <span class="indicator-item badge badge-${singlePost.isActive===true?'success':'error'}"></span>
+            <span class="indicator-item badge badge-${singlePost.isActive === true ? 'success' : 'error'}"></span>
             <div class="place-items-start h-[250px]">
                 <img class="w-32 h-32  rounded-3xl"
                 src="${singlePost?.image || 'error'}" />
@@ -20,6 +20,10 @@ const displayAllPosts = (allPosts) =>{
             </div>
 
             <div>
+            <div class="flex gap-6">
+            <p># <span>${singlePost.category} </span></p> 
+            <p>Author : <span>${singlePost.author.name}</span></p>
+            </div>
             <h1 class="text-xl font-bold">${singlePost?.title || 'error'}</h1>
             <p class="py-6">${singlePost?.description || 'error'}</p>
             <div class="flex justify-around items-center">
@@ -29,7 +33,7 @@ const displayAllPosts = (allPosts) =>{
                 <p><i class="fa-regular fa-clock"></i><span id="icons"> ${singlePost?.posted_time || 'error'} </span>min</p>
                 </div> 
                 <div>
-                <button  class="btn p-4 text-white text-sm bg-[#797DFC] rounded-full"><i class="fa-regular fa-envelope"></i></button>
+                <button onclick = "btnClick('${singlePost?.title}','${singlePost?.view_count}' )" class="btn p-4 text-white text-sm bg-[#10B981] rounded-full"><i class="fa-regular fa-envelope"></i></button>
                 </div>
             </div>
             </div>
@@ -37,10 +41,23 @@ const displayAllPosts = (allPosts) =>{
         </div> 
       
       `;
-      latestPostContainer.appendChild(cardDiv2)
+    latestPostContainer.appendChild(cardDiv2)
 
-      
-        // Div Append
 
-    });
+    // Div Append
+  });
+}
+let count = 0;
+btnClick = (title, views) => {
+  count++;
+  const counter = document.getElementById('clickCounter')
+  counter.innerText = count;
+  const rightContainer = document.getElementById('dynamic-rightSide')
+  const newCard = document.createElement('div')
+  newCard.innerHTML =`
+  <div class=" flex gap-10 mt-10 shadow-xl p-6 rounded-xl">
+  <h1 class="text-xl font-bold">${title}<h1>
+  <p><i class="fa-solid fa-eye"></i><span>${views}</span></p>
+</div>`
+rightContainer.appendChild(newCard)
 }
